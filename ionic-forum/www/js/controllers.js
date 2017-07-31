891044b6ecc201228206bca5997f7469e01aa257
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2017-07-31 08:14:20 +0530

    initial  Commit

---
+++ ionic-forum/www/js/controllers.js
@@ -0,0 +1,53 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function ($scope) {})
+
+.controller('HomeCtrl', function ($scope, $ionicModal, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function (chat) {
+    Chats.remove(chat);
+  };
+
+  $ionicModal.fromTemplateUrl('templates/new-topic.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+  $scope.openNewTopicModal = function () {
+    $scope.modal.show();
+  };
+  $scope.closeNewTopicModal = function () {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function () {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function () {
+    // Execute action
+  });
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
