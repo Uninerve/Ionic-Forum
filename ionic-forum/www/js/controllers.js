909e461026aea75c01dc84ecab8c3ca08c0ce059
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2017-08-01 12:03:05 +0530

    Add reply model

--- ionic-forum/www/js/controllers.js
+++ ionic-forum/www/js/controllers.js
@@ -44,6 +44,32 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+
+  $ionicModal.fromTemplateUrl('templates/reply-to-topic.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openReplyTopicModal = function () {
+    $scope.modal.show();
+  };
+  $scope.closeReplyTopicModal = function () {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function () {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function () {
+    // Execute action
+  });
 })
 
 .controller('AccountCtrl', function ($scope) {
